e5f45e3bee42631407636d7683ae2625d99c240d
princevip1 <princevip@gmail.com>
2022-02-05 20:48:23 +0800

    firt

--- src/components/pages/phoneNumber/PhoneNumber.jsx
+++ src/components/pages/phoneNumber/PhoneNumber.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import bgImage from "../../../image/bg.png"
 
 const PhoneNumber = () => {
+    const backgroungImage={
+        backgroundImage: `url(${bgImage})`,
+        // backgroundRepeat: "no-repeat",
+        // backgroundSize: "100% 100%",
+        // padding:"380px 20px"
+        height: "100%",
+
+  /* Center and scale the image nicely */
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
+         
+    }
+
+
     return (
-        <div>
+        <div style={backgroungImage}>
             <p>this is phone number</p>
         </div>
     );
