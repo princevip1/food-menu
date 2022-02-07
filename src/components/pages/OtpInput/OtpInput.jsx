852849591363c18111ad28a7ead5374661a1f0be
princevip1 <princevip@gmail.com>
2022-02-07 20:05:56 +0800

    jatadur hoise

--- src/components/pages/OtpInput/OtpInput.jsx
+++ src/components/pages/OtpInput/OtpInput.jsx
@@ -5,10 +5,14 @@ import { BiMessage } from "react-icons/bi";
 
 const OtpInput = () => {
     const [OTP, setOTP] = useState("");
+    const handleOtp = () => {
+        console.log(OTP);
+    }
+    
 
     const backgroungImage={
         backgroundImage: `url(${bgImage})`,
-        // backgroundAttachment: "fixed"
+        backgroundAttachment: "fixed",
         backgroundRepeat: "no-repeat",
         backgroundSize:"cover",
         width:"100%",
@@ -23,8 +27,13 @@ const OtpInput = () => {
             <span className='text-white text-6xl flex justify-center'><BiMessage/></span>
             <h1 className='text-white text-2xl font-bold my-3'> SMS password</h1>
             <p  className='text-white text-lg my-3'>Insert a four-digit code from SMS.</p>
-            <OTPInput inputClassName="bg-white/30 my-5 p-8 text-white text-3xl rounded  focus:outline-none focus:border-sky-300 focus:ring-sky-300 focus:ring-1 focus:text-white" className=" ml-4" value={OTP} onChange={setOTP} autoFocus OTPLength={4} otpType="number" disabled={false}  />
-            <button className="rounded bg-gradient-to-t from-orange-600 to-orange-400 w-80 text-md py-3 my-5 text-white font-bold">Apply</button>
+            <OTPInput inputClassName="bg-white/30 my-5 text-white text-3xl rounded focus:outline-none focus:border-sky-300 focus:ring-sky-300 focus:ring-1 focus:text-white" className=" ml-4" value={OTP} onChange={setOTP} autoFocus OTPLength={4} otpType="number" disabled={false}  />
+            {OTP.length === 4? (
+                <button onClick={handleOtp} className="rounded bg-gradient-to-t from-orange-600 to-orange-400 w-80 text-md py-3 my-5 text-white font-bold">Apply</button>
+            ): (
+                <button disabled className="rounded bg-gradient-to-t from-gray-600 to-gray-400 w-80 text-md py-3 my-5 text-white font-bold">Apply</button>
+            )}
+            
             </div>
         </div>
     );
